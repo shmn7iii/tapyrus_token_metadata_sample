22c62af8708a06df88516c99c2bcbdfe6d3812a7
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-04-14 22:24:48 +0900

    add: static_pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get  '/ntfs/show',    'nfts/show'
-  get  '/nfts/new',     'nfts/new'
-  post '/nfts/create',  'nfts/create'
+  root   'static_pages#home'
+  get    '/ntfs/show',     to: 'nfts#show'
+  get    '/nfts/new',      to: 'nfts#new'
+  post   '/nfts/create',   to: 'nfts#create'
 end
