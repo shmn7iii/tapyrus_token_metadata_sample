b21e8c7f7dafc0d34431ef6649f96b1f54c88a13
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-04-15 08:35:49 +0900

    fix: typo in route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root   'static_pages#home'
-  get    '/ntfs/show',     to: 'nfts#show'
+  get    '/nfts/show',     to: 'nfts#show'
   get    '/nfts/new',      to: 'nfts#new'
   post   '/nfts/create',   to: 'nfts#create'
 end
