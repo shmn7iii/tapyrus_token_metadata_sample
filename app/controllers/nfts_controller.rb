84565cae77ef772522e5cc5bdc88e8f6a514eb38
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-04-16 14:46:48 +0900

    update: enable IPFS uploading

--- app/controllers/nfts_controller.rb
+++ app/controllers/nfts_controller.rb
@@ -1,17 +1,35 @@
 class NftsController < ApplicationController
+  require 'ipfs-api'
+
   skip_before_action :verify_authenticity_token
 
   def new; end
 
   def create
-    metadata = params[:metadata]
+    # write file
+    upload_file = params[:upload_file]
+    upload_dir = Rails.root.join("tmp", "storage")
+    upload_file_path = upload_dir + Time.now.to_i.to_s
+    File.binwrite(upload_file_path, upload_file.read)
+
+    # upload to ipfs
+    ipfs = IPFS::Connection.new('http://ipfs:5001')
+    ipfs.add File.new(upload_file_path) do |node|
+      @cid = node.hash
+    end
 
     # load wallet
-    wallet = Glueby::Wallet.load(ENV['WALLET_ID'])
-
     begin
-      # create token
-      token = Glueby::Contract::Token.issue_nft_with_metadata(wallet:, prefix: '', metadata:)
+      wallet = Glueby::Wallet.load(ENV['WALLET_ID'])
+    rescue Glueby::Internal::Wallet::Errors::WalletNotFound
+      flash.now[:danger] = 'Wallet not found. Are you run ./bin/rails ttms:init? Run it and restart server!'
+      render 'new'
+      return
+    end
+
+    # create token
+    begin
+      token = Glueby::Contract::Token.issue_nft_with_metadata(wallet: wallet, prefix: '', metadata: @cid)
     rescue Glueby::Contract::Errors::InsufficientFunds
       # deposit
       block = Glueby::Internal::RPC.client.generatetoaddress(1, wallet.internal_wallet.receive_address,
