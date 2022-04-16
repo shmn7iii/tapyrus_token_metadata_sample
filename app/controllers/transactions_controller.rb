9d28ed817c4b177936a07b080a5fcf76163cda72
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-04-16 17:48:28 +0900

    update: add search button to header

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,4 +1,22 @@
 class TransactionsController < ApplicationController
+
+  def search
+    txid = nil
+    listunspents = Glueby::Wallet.load(ENV['WALLET_ID']).internal_wallet.list_unspent
+    listunspents.each do |listunspent|
+      if listunspent[:color_id] == params[:color_id]
+        txid = listunspent[:txid]
+      end
+    end
+
+    if txid
+      redirect_to transactions_show_path(txid: txid)
+    else
+      flash[:danger] = 'No match :('
+      redirect_to root_path
+    end
+  end
+
   def show
     txid = params[:txid]
     rawtransaction = Glueby::Internal::RPC.client.getrawtransaction(txid.to_s)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root   'static_pages#home'
 
-  get    '/transactions/show', to: 'transactions#show'
+  post   '/transactions/search',  to: 'transactions#search'
+  get    '/transactions/show',    to: 'transactions#show'
 
   get    '/nfts/new',      to: 'nfts#new'
   post   '/nfts/create',   to: 'nfts#create'
