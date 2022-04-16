a7ae6b7f30f5dc305de32fe55ae1363e05f7a79d
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-04-16 20:52:15 +0900

    update: transaction#search can search from txid or color_id

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,23 +1,27 @@
 class TransactionsController < ApplicationController
   def search
     txid = nil
-    listunspents = Glueby::Wallet.load(ENV['WALLET_ID']).internal_wallet.list_unspent
-    listunspents.each do |listunspent|
-      txid = listunspent[:txid] if listunspent[:color_id] == params[:color_id]
+    if params[:color_id].start_with?('c1', 'c2', 'c3')
+      listunspents = Glueby::Wallet.load(ENV['WALLET_ID']).internal_wallet.list_unspent
+      listunspents.each do |listunspent|
+        txid = listunspent[:txid] if listunspent[:color_id] == params[:color_id]
+      end
+    else
+      txid = params[:color_id]
     end
 
-    if txid
-      redirect_to transactions_show_path(txid: txid)
-    else
-      flash[:danger] = 'No match :('
-      redirect_to root_path
-    end
+    redirect_to transactions_show_path(txid: txid)
   end
 
   def show
     txid = params[:txid]
-    rawtransaction = Glueby::Internal::RPC.client.getrawtransaction(txid.to_s)
-    @decodedtransaction = Glueby::Internal::RPC.client.decoderawtransaction(rawtransaction)
-    @json = JSON.pretty_generate(@decodedtransaction)
+    begin
+      rawtransaction = Glueby::Internal::RPC.client.getrawtransaction(txid.to_s)
+      @decodedtransaction = Glueby::Internal::RPC.client.decoderawtransaction(rawtransaction)
+      @json = JSON.pretty_generate(@decodedtransaction)
+    rescue
+      flash[:danger] = 'No transactions found :('
+      redirect_to root_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root   'static_pages#home'
 
-  post   '/transactions/search',  to: 'transactions#search'
   get    '/transactions/show',    to: 'transactions#show'
+  post   '/transactions/search',  to: 'transactions#search'
 
   get    '/nfts',         to: 'nfts#index'
   get    '/nfts/new',     to: 'nfts#new'
